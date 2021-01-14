78f7b18b534c4fcf55dbc5bdb2a8835d7184c537
chronoby <1076947334@qq.com>
2021-01-14 23:37:50 +0800

    contract: struct define

--- src/eth/crowdfunding.sol
+++ src/eth/crowdfunding.sol
@@ -1,3 +1,26 @@
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
 
-pragma solidity >=0.7.0 <0.8.0;
+contract crowdfunding {
+    struct investor {
+        address payable addr;
+        uint amount;
+    }
+
+    struct funding {
+        address payable creator_addr;
+        address payable[] participators_addr;
+        mapping(uint => investor) id_to_investor;
+
+        uint start_time;
+        uint end_time;
+        string intro;
+        uint amount_all;
+        uint amount_get;
+    }
+
+    struct request {
+        string purpose;
+        uint amount;
+    }
+}
